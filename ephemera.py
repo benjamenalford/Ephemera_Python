26f741a3dc65059d3bae4a51daf158d02cd188db
Benjamen Alford <benjamen.alford@gmail.com>
2024-01-04 09:51:10 -0600

    cleanup

--- ephemera.py
+++ ephemera.py
@@ -39,7 +39,7 @@ def view_api_collection(collection):
     collection = db[collection]
     data = collection.find()
     data = [item for item in data]
-    return render_template('view_collection.html',data=data)
+    return render_template('view_collection.html',data=data,collection_name=collection)
 
 @app.route("/api/<collection>/add", methods=['POST'])
 def api_collection_add(collection):
